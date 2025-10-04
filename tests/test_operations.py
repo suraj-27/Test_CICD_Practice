e50816d7d0212df870d1936d031d931b9af98b0e
surajupade@gmail.com <surajupade@gmail.com>
2025-10-04 22:10:08 +0530

    test cases updated

---
+++ tests/test_operations.py
@@ -0,0 +1,11 @@
+from src.math_operations import add, sub
+
+def test_add():
+    assert add(3,2) == 5
+    assert add(-1, 3) == 2
+    
+def test_sub():
+    assert sub(3,2) == 1
+    assert sub(5,7) == -2
+    assert sub(-1,4) == -5
+    
